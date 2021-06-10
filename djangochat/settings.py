ae8bd0d9a7d6e3a3f8e66150303c38178bf835a2
Damir Bakty <damir050602@gmail.com>
2021-06-10 12:59:19 +0500

    10.06.2021 (1)

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -1,4 +1,3 @@
-
 import os
 from pathlib import Path
 
@@ -77,7 +76,7 @@ REST_FRAMEWORK = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
@@ -117,6 +116,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
-STATIC_URL = '/static/',
 STATIC_ROOT = os.path.join(BASE_DIR, 'static'),
+STATIC_URL = '/static/',
